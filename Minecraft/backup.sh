e396adddf2bf6a882ddbe0482dabcc3e27c815c5
Jihyuk Lee <lead@sver.dev>
2024-11-13 17:58:56 +0900

    delete :: 사용하지 않는 서비스 제거

--- Minecraft/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-minecraft_folder="/home/ubuntu/homelab/Minecraft/minecraft-data"
-backup_folder="/home/ubuntu/homelab/Minecraft/mc-backups"
-backup_file="$backup_folder/mc_backup_$(date +\%Y\%m\%d_\%H\%M).tar.gz"
-tar -czf "$backup_file" -C "$minecraft_folder" .
-
-echo "마인크래프트 서버 백업이 완료되었습니다. 백업 파일: $backup_file"
